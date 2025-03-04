41204f69eab220d9efca15456422eb6a5d2a6b5e
CAMILA_RODRIGUEZ_RUIZ <a.22308051281300@cbtis128.edu.mx>
2025-03-04 16:41:28 +0000

    p 10-5

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,72 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MiContenedorApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MiContenedorApp extends StatelessWidget {
+  const MiContenedorApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Ejemplo 18 Container"),
+          centerTitle: true,
+          titleTextStyle: const TextStyle(color: Colors.black, fontSize: 20),
+          backgroundColor: const Color(0xffffbbb6),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Camila Rodriguez Ruiz\nMatricula: 22308051281300',
+                textAlign: TextAlign.center, // Centra el texto
+                style: TextStyle(
+                  fontSize: 16,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.black,
+                ),
+              ),
+              // Primer contenedor
+              Container(
+                //to align its child
+                child: MyCardContainer(),
+              ),
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+              SizedBox(height: 20), // Espacio entre los contenedores
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+              // Segundo contenedor
+            ],
+          ),
         ),
       ),
+    );
+  } // fin widgets
+} // fin clase MiContenedorApp
+
+class MyCardContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(30),
+      height: 300,
+      width: 300,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30),
+        gradient: new LinearGradient(
+          colors: [
+            Color(0xff72dcfd),
+            Color(0xff256ac5),
+          ],
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
+          stops: [0.25, 0.90],
+        ),
+      ),
+      alignment: Alignment.centerLeft, //to align its child
+      padding: EdgeInsets.all(20),
     );
   }
 }
